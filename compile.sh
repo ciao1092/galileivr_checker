9052c3a58da906976fbfc4bb96393ea2b5b38661
Carlo <100089228+ciao1092@users.noreply.github.com>
2023-12-29 20:21:54 +0100

    Fix composer / compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+if [ ! -f "./vendor/bin/box" ]; then
+	composer require --dev bamarni/composer-bin-plugin
+	composer bin box require --dev humbug/box
+fi
+
 ./vendor/bin/box compile || exit $?
 mv index.phar galileivr_checker
